316fe5c3b7593b602327217f93f991d11a93b338
Alexandar <AlexandarTankov@gmail.com>
2014-11-03 14:19:05 +0200

    Update tests on animal

--- animal_test.py
+++ animal_test.py
@@ -2,11 +2,10 @@ import unittest
 
 from animal import Animal
 
-
 class Animal_test(unittest.TestCase):
 
     def setUp(self):
-        self.my_animal = Animal("bear", 5, "puh", "m", 150)
+        self.my_animal = Animal("bear", 5, "puh", "m", 150, 10)
 
     def test_init(self):
         self.assertEqual(self.my_animal.species, "bear")
@@ -14,6 +13,28 @@ class Animal_test(unittest.TestCase):
         self.assertEqual(self.my_animal.name, "puh")
         self.assertEqual(self.my_animal.gender, "m")
         self.assertEqual(self.my_animal.weight, 150)
+        self.assertTrue(self.my_animal.alive)
+        self.assertEqual(self.my_animal.life_expectancy, 10)
+
+    def test_grow(self):
+        self.my_animal.grow()
+        self.assertEqual(self.my_animal.age, 6)
+        self.assertEqual(self.my_animal.weight, 151)
+
+    def test_eat(self):
+        self.my_animal.eat()
+        self.assertEqual(self.my_animal.weight, 151)
+
+    def test_should_die(self):
+        flag_die = False
+        flag_alive = False
+        for i in range(0, 10):
+            self.my_animal.should_die()
+            if not self.my_animal.alive:
+                flag_die = True
+            else:
+                flag_alive = True
+        self.assertTrue(flag_die and flag_alive)
 
 if __name__ == '__main__':
     unittest.main()
