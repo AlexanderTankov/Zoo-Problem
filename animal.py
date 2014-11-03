238525ead1e10154cab60c8daef2f7e0e26b4e96
Daniel Ivanov <d.ivanov456@gmail.com>
2014-11-03 14:17:00 +0200

    animal class

--- animal.py
+++ animal.py
@@ -1,7 +1,25 @@
+from random import random
+
+
 class Animal:
-    def __init__(self, species, age, name, gender, weight):
+    def __init__(self, species, age, name, gender, weight, life_expectancy):
         self.species = species
         self.age = age
         self.name = name
         self.gender = gender
         self.weight = weight
+        self.alive = True
+        self.life_expectancy = life_expectancy
+
+    def grow(self):
+        self.age += 1
+        self.weight += 1
+
+    def eat(self):
+        self.weight += 1
+
+    def should_die(self):
+        chance_of_dying = self.age / self.life_expectancy
+        num = random.random(0, 1)
+        if num < chance_of_dying:
+            self.alive = False
