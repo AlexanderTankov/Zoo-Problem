cd792b4c7e09249cf22a58eb1c34cad0d2f04ae3
Daniel Ivanov <d.ivanov456@gmail.com>
2014-11-04 10:51:52 +0200

    3 functions for zoo class

--- zoo.py
+++ zoo.py
@@ -1,5 +1,26 @@
 class Zoo:
+    INCOME_FROM_ANIMAL = 60
+    MEAT = 4
+    BAMBOO = 2
+
     def __init_(self, animals, capacity, budged):
-        self.animals = animals
+        self.animals = []
         self.capacity = capacity
         self.budged = budged
+
+    def add_new_animal(self, new_animal):
+        if self.capacity == len(self.animals):
+            raise ValueError("The zoo is full")
+        for animal in self.animals:
+            if animal.species == new_animal.species and animal.name == new_animal.name:
+                raise ValueError("Choose another name for the new animal")
+        self.animals.append(new_animal)
+
+    def daily_income(self):
+        income = self.INCOME_FROM_ANIMAL * len(self.animals)
+        self.budged += income
+        return income
+
+    def death(self, animal):
+        if not animal.should_die():
+            self.animals.remove(animal)
